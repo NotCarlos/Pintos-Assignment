a5f1033504cd61ecf15236910e95f8ee3e30d5f0
b2-elljones <benjamin2.ell-jones@live.uwe.ac.uk>
2020-12-23 12:56:06 -0500

    sys_exit sys_create

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -39,6 +39,39 @@ static void syscall_handler (struct intr_frame *f UNUSED)
 			f->eax = open(file);
 			}
 			break;
+		case SYS_EXIT: // completed 21/12/2020
+  			printf(" "); // prvents error
+  			int status = *((int*)f->esp + 1);// pulling status out of stack
+  			struct thread* current = thread_current();// getting the current thread
+  			current->exitCode = status; // Set the status of the thread to the exit code
+  			thread_exit();// terminate current thread
+  			break;
+
+  		case SYS_CREATE:// completed 21/12/2020
+			printf(" "); // prvents error
+  			const char * file = ((const char*)f->esp + 1); // pull file name out of stack 
+  			unsigned initial_size = *((unsigned*)f->esp + 2);// pull file size out of stack
+  			bool isCreated = false;
+
+  			isCreated = filesys_create(file, initial_size); //create file return true if file created
+  			f->eax = isCreated; // add return to eax register 
+  			printf("Is file created?: %d",isCreated);
+  		break;
+
+  		case SYS_FILESIZE: // need to create file descripter 
+			printf(" ");
+  			struct file * fileToProccess = get_user();
+  		
+  		//int fd = *((int*)f->esp + 1);
+  			f->eax = file_length(fileToProccess);
+  			break;
+  		
+  		case SYS_SEEK:
+  			printf(" ");
+  			//int fd = *((int*)f->esp + 1);
+  			//unsigned position = *((unsigned*)f->esp + 2);
+
+  			break;
 		
 	}
 	// wait((pid_t)p);
